d3d1efcc7f800a59ecbb61dd79c14ac400d9910f
JfffV <ultionem14@gmail.com>
2024-02-04 23:15:30 +0100

    added delete feature

--- my-app/src/components/body.js
+++ my-app/src/components/body.js
@@ -10,28 +10,42 @@ function Body(props) {
   }, []);
   const moveGame = (title, direction, name) => {
     const games = JSON.parse(JSON.stringify(props.games));
-    let keyToAdd = '';
-  
-    if (title === 'todo' || title === 'done') {
-      keyToAdd = 'inprogress';
-    } else {
-      keyToAdd = direction === 'left' ? 'todo' : 'done';
-    }
+    let keyToAdd = "";
 
+    if (title === "todo" || title === "done") {
+      keyToAdd = "inprogress";
+    } else {
+      keyToAdd = direction === "left" ? "todo" : "done";
+    }
     games[title] = games[title].filter((game) => game.name !== name);
     games[keyToAdd].push({ name: name });
-    props.setGames(games);  
+    props.setGames(games);
   };
-
+  const deletGame = (title, name) => {
+    const games = JSON.parse(JSON.stringify(props.games));
+    games[title] = games[title].filter((game) => game.name !== name);
+    props.setGames(games);
+  };
   return (
     <div className="games_container">
-      <Column games={props.games.todo} title="todo" moveGame={moveGame} />
+      <Column
+        games={props.games.todo}
+        title="todo"
+        moveGame={moveGame}
+        deletGame={deletGame}
+      />
       <Column
         games={props.games.inprogress}
         title="inprogress"
         moveGame={moveGame}
+        deletGame={deletGame}
       />
-      <Column games={props.games.done} title="done" moveGame={moveGame} />
+      <Column
+        games={props.games.done}
+        title="done"
+        moveGame={moveGame}
+        deletGame={deletGame}
+      />
     </div>
   );
 }
--- my-app/src/components/column.js
+++ my-app/src/components/column.js
@@ -1,5 +1,6 @@
 import { Button } from "react-bootstrap";
 import { useEffect } from "react";
+import "./column.css";
 import "./body.css";
 
 function Column(props) {
@@ -7,27 +8,50 @@ function Column(props) {
     console.log(props);
   }, []);
 
-  
-
   return (
     <div className="border">
       <p className="title_game">{props.title}</p>
       {props.games.map((game) => (
         <div className="for_games">
-          {props.title === 'inprogress' || props.title === 'done' ?(  
+           {props.title === "inprogress" || props.title === "done" ? (
+            <>
+              <Button className="moveGames"
+                onClick={() => {
+                  props.moveGame(props.title, "left", game.name);
+                }}
+                variant="primary"
+              >
+                <i class="bi bi-arrow-left"></i>
+              </Button>
             
-          <Button onClick={()=>{props.moveGame(props.title, 'left', game.name)}} variant="primary">
-            <i class="bi bi-arrow-left"></i>
-          </Button> ) : null}
-          <p className="game_name"> {game.name} </p>
-          
-          {props.title === 'todo' || props.title === 'inprogress'  ?(
-          <Button onClick={()=>{props.moveGame(props.title, 'right', game.name)}} variant="primary">
-            <i class="bi bi-arrow-right"></i>
-          </Button>) : null}
-          
+            </>
+          ) : null}
 
-          </div>
+          <p className="game_name"> {game.name} </p>
+
+          {props.title === "todo" || props.title === "inprogress" ? (
+            <>
+             
+              <Button
+                onClick={() => {
+                  props.moveGame(props.title, "right", game.name);
+                }}
+                variant="primary"
+              >
+                <i class="bi bi-arrow-right"></i>
+              </Button>
+            </>
+          ) : null}
+            <Button className="deletGame"
+                onClick={() => {
+                 props.deletGame(props.title, game.name)
+                }}
+                variant="danger"
+              >
+                <i class="bi bi-x"></i>
+              </Button>
+         
+        </div>
       ))}
     </div>
   );
